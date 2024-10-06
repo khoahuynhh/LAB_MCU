ad3c2b000b40a08649df8fd82a4b0ec852db62d6
DESKTOP-Q2S85GV\Admin <huynhbuingockhoa@gmail.com>
2024-10-06 23:38:41 +0700

    update Lab2_6

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -56,30 +56,6 @@ static void MX_GPIO_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-GPIO_TypeDef *SEG_PORTS[12] = { SEG_0_GPIO_Port, SEG_1_GPIO_Port,
-SEG_2_GPIO_Port, SEG_3_GPIO_Port, SEG_4_GPIO_Port, SEG_5_GPIO_Port,
-SEG_6_GPIO_Port };
-uint16_t SEG_PINS[12] = { SEG_0_Pin, SEG_1_Pin, SEG_2_Pin, SEG_3_Pin,
-SEG_4_Pin, SEG_5_Pin, SEG_6_Pin };
-GPIO_PinState LEDS_state[10][7] = { { 0, 0, 0, 0, 0, 0, 1 }, { 1, 0, 0, 1, 1, 1,
-		1 }, { 0, 0, 1, 0, 0, 1, 0 } };
-void set_LEDS(GPIO_PinState *L_LEDS_state) {
-	for (int i = 0; i < 7; i++) {
-		HAL_GPIO_WritePin(SEG_PORTS[i], SEG_PINS[i], L_LEDS_state[i]);
-	}
-}
-void display7SEG(int number) {
-	switch (number) {
-	case 1:
-		set_LEDS(LEDS_state[1]);
-		break;
-	case 2:
-		set_LEDS(LEDS_state[2]);
-		break;
-	default:
-		break;
-	}
-}
 
 /* USER CODE END 0 */
 
@@ -117,25 +93,11 @@ int main(void) {
 
 	/* Infinite loop */
 	/* USER CODE BEGIN WHILE */
-	setTimer(0, 500);
-	int counter = 0;
+	setTimer(0, 1000);
 	while (1) {
 		if (timer_flag[0] == 1) {
-			switch (counter) {
-			case 0:
-				HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, 0);
-				HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, 1);
-				display7SEG(1);
-				counter++;
-				break;
-			case 1:
-				HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, 1);
-				HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, 0);
-				display7SEG(2);
-				counter = 0;
-				break;
-			}
-			setTimer(0, 500);
+			HAL_GPIO_TogglePin(LED_RED_GPIO_Port, LED_RED_Pin);
+			setTimer(0, 2000);
 		}
 		/* USER CODE END WHILE */
 
